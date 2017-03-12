9f4520d120e7ed0471a01b1f022e6c5597b37462
chrischne <chrischne@gmail.com>
2017-03-12 23:26:39 +0100

    changed output name

--- generate.sh
+++ generate.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")"
 
 rm cv.pdf
 
-jekyll build && sleep 3 && prince _site/index.html -o cv.pdf && sleep 3 && open cv.pdf
+jekyll build && sleep 3 && prince _site/index.html -o cv_christian_schneider.pdf && sleep 3 && open cv_christian_schneider.pdf
--- generate_no_open.sh
+++ generate_no_open.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")"
 
 rm cv.pdf
 
-jekyll build && sleep 1 && prince _site/index.html -o cv.pdf  && sleep 2 && killall Terminal
+jekyll build && sleep 1 && prince _site/index.html -o cv_christian_schneider.pdf  && sleep 2 && killall Terminal
